a6744c027c9a94f5ce08922c48942269e7be30e0
Ryo <kanadajinlee@yahoo.ca>
2017-03-27 15:55:27 -0500

    Created the SHOW route & file, and now you can click on the individual blog articles (動画＃２５６)

--- app.js
+++ app.js
@@ -20,12 +20,13 @@ let blogSchema = new mongoose.Schema({
     body: String,
     created: {type: Date, default: Date.now} //So if someone makes a blog post without having this, it will default to the current date. Could be useful for other things too, like the image URL having a placeholder if nothing was provided
 })
+
 let Blog = mongoose.model("Blog", blogSchema);
 
 // Blog.create({
-//     title: "Test Blog",
-//     image: "http://cdn.wallpapersafari.com/17/99/knHlCG.jpg",
-//     body: "Look at this pic I found! It's the first on this blog.",
+//     title: "First Post",
+//     image: "https://images.unsplash.com/photo-1473496169904-658ba7c44d8a?dpr=2&auto=compress,format&fit=crop&w=767&h=511&q=80&cs=tinysrgb&crop=",
+//     body: "I made this from inside the app.js file. Had to delete the DB because the images weren't httpS. Ugh...",
 // })
 
 
@@ -52,15 +53,26 @@ app.get("/blogs/new", (req, res) => {
 
 app.post("/blogs", (req, res) => {
     //create blog post (stick into DB)
-    Blog.create(req.body.blog, (err, newBlog)=>{
+    Blog.create(req.body.blog, (err, newBlog) => {
         if(err){
-            console.log("Sorry, there was an error...")
             res.render("new");
         } else {
+            //then, redirect to the index
             res.redirect("/blogs");
         }
     });
-})
+});
+
+//SHOW ROUTE
+app.get("/blogs/:id", (req, res) => {
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog}); //this "blog" is whatever we found inside the SHOW route, because we looked in the DB for the ID. And if we found it (and we almost definitely will then pass foundBlog through (as "blog", like I said) to show.ejs
+        }
+    })
+});
 
 
 app.listen(3000, function () {
